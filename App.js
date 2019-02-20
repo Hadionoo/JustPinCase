20ea47715cc17df165304332f7f6344feeb3320e
Isaac Ng <32532726+byrrice@users.noreply.github.com>
2019-02-20 04:09:45 -0500

    added database

--- App.js
+++ App.js
@@ -6,6 +6,19 @@ import Map from "./components/Map";
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import Login from "./components/Login";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAomUzviEzRitHhTK1IR9LJbfhU6_9CzBk",
+  authDomain: "justpincase-c0785.firebaseapp.com",
+  databaseURL: "https://justpincase-c0785.firebaseio.com",
+  projectId: "justpincase-c0785",
+  storageBucket: "justpincase-c0785.appspot.com",
+  messagingSenderId: "357323316713"
+};
+if (!firebase.app.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+var db = firebase.database();
+
 const TabNavigator = createBottomTabNavigator({
   Home: {
     screen: Map,
